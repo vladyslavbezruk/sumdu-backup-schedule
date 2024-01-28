213aa559f10dff7491948c4b9c5b36a5e8494dc6
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-28 17:54:01 +0200

    Added.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os.path
+import time
 from datetime import datetime, timedelta
+
+import requests
 
 from src import config
 from src import files
@@ -23,6 +26,9 @@ def update_groups():
 def backup_schedules():
     groups = files.load_file(paths.groups_file_path)
     i = 0
+    count_ok = 0
+    count_error = 0
+    count_all = 0
 
     for group_code in groups:
         i += 1
@@ -37,32 +43,74 @@ def backup_schedules():
             flag = False
 
             try:
-                filename = 'schedule-' + group_code + '.json'
+                # json
+                filename_json = 'schedule-' + group_code + '.json'
 
-                path = os.path.join(paths.backups_file_path, str(datetime.now().date()))
+                path_json = os.path.join(paths.backups_file_path, 'json', str(datetime.now().date()))
 
-                if not os.path.exists(path):
-                    os.mkdir(path)
+                if not os.path.exists(path_json):
+                    os.mkdir(path_json)
 
-                path = os.path.join(path, filename)
+                path_json = os.path.join(path_json, filename_json)
 
-                url = config.site_schedules + group_code + '&date_end=' + formatted_date
+                url_json = config.site_schedules + group_code + '&date_end=' + formatted_date
 
-                schedule = jsons.download_json(url)
+                schedule_json = jsons.download_json(url_json)
 
-                files.save_file(schedule, path)
+                files.save_file(schedule_json, path_json)
+
+                # pdf
+
+                filename_pdf = str(groups[group_code]).replace('/', '-') + '.pdf'
+
+                path_pdf = os.path.join(paths.backups_file_path, 'pdf', str(datetime.now().date()))
+
+                if not os.path.exists(path_pdf):
+                    os.mkdir(path_pdf)
+
+                path = os.path.join(path_pdf, filename_pdf)
+
+                url_pdf = config.site_pdf_schedules + group_code + '&date_end=' + formatted_date
+
+                schedule_pdf = requests.get(url_pdf)
+
+                if schedule_pdf.status_code == 200:
+                    with open(path, 'wb') as file:
+                        file.write(schedule_pdf.content)
+                else:
+                    flag = True
 
             except:
                 print('Не вдалося зробити бекап для групи ' + groups[group_code])
 
                 flag = True
 
+                count_error += 1
+
             if not flag:
                 print('Успішно зроблено бекап для групи ' + groups[group_code])
 
+                count_ok += 1
+
             print('Загальний прогрес ' + str(round(100.0 * i / len(groups), 3)) + '%')
 
+            count_all += 1
+
+    print('Операцію завершено')
+    print('Всього груп ' + str(count_all))
+    print('Успішно збережено ' + str(count_ok))
+    print('Не збережено ' + str(count_error))
+    input()
+
+
+start_time = time.time()
 
 update_groups()
 
 backup_schedules()
+
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+
+print(f"Час виконання: {elapsed_time} секунд")
